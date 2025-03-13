7730a7a25cc3fe08e2d17cb3181104ec462cdf97
Juanma <jmgalang07@iesalbarregas.es>
2025-03-13 17:53:12 +0100

    Initial commit

---
+++ src/CameraPosition.js
@@ -0,0 +1,11 @@
+export const CAMERA_POSITIONS = [
+  { x: 0, y: 150, z: 900, rotationX: 0, rotationY: 0, rotationZ: 0 },
+  { x: 350, y: 150, z: 850, rotationX: 0, rotationY: -0.2, rotationZ: 0 },
+  { x: -350, y: 150, z: 850, rotationX: 0, rotationY: 0.2, rotationZ: 0 },
+  { x: 0, y: 200, z: 750, rotationX: -0.1, rotationY: 0, rotationZ: 0 },
+  { x: 300, y: 180, z: 700, rotationX: 0, rotationY: -0.3, rotationZ: 0 },
+  { x: -200, y: 180, z: 700, rotationX: 0, rotationY: 0.3, rotationZ: 0 },
+  { x: 0, y: 250, z: 600, rotationX: -0.2, rotationY: 0, rotationZ: 0 },
+  { x: 800, y: 300, z: 500, rotationX: 0, rotationY: -0.5, rotationZ: 0 },
+  { x: -800, y: 300, z: 500, rotationX: 0, rotationY: 0.5, rotationZ: 0 }
+];
